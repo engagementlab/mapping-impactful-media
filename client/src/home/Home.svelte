f5569eab0c86b268b3d8411a1da0b381627aa392
Johnny Richardson <johnny.c.rich@gmail.com>
2021-03-17 14:14:25 -0400

    feat(client): Image component usage on home.

--- client/src/home/Home.svelte
+++ client/src/home/Home.svelte
@@ -1,5 +1,9 @@
 <script>
     import { onMount } from 'svelte';
+    import Image from '../shared/Image.svelte';
+    const src = '/mapping-impactful-media/img/home-artwork';
 </script>
 
 <div>Coming Soon</div>
+
+<Image img_id="home-artwork" />
