13bff57d560e1a75a095cd32635d743fd4524d33
Johnny Richardson <johnny.c.rich@gmail.com>
2021-03-19 12:14:08 -0400

    feat(client): Tailwind, Footer components.

---
+++ client/src/shared/Footer.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+</script>
+
+<footer class="absolute bottom-0 min-w-full bg-peach">
+    <div class="container mx-auto bottom-3 py-8">Footer here</div>
+</footer>
+
+<style type="text/scss">
+</style>
