537e7f3ecd154bf706cd81327307682df23d334a
Johnny Richardson <johnny.c.rich@gmail.com>
2021-03-25 12:39:02 -0400

    chore(api): eslint rules change.

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,24 +1,25 @@
 module.exports = {
     env: {
-      commonjs: true,
-      es6: true,
-      node: true,
+        commonjs: true,
+        es6: true,
+        node: true,
     },
     extends: ['airbnb-base', 'eslint-config-prettier'],
     globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
     },
     parserOptions: {
-      ecmaVersion: 2018,
+        ecmaVersion: 2018,
     },
     rules: {
-      semi: ['error', 'always'],
-      indent: ['error', 4],
-      'arrow-parens': [2, 'as-needed'],
-      'operator-linebreak': ['error', 'after'],
-      'comma-dangle': ['error', {
-        objects: 'always'
-      }],
+        semi: ['error', 'always'],
+        indent: ['error', 4],
+        'arrow-parens': [2, 'as-needed'],
+        'operator-linebreak': ['error', 'after'],
+        'comma-dangle': ['error', {
+            objects: 'always',
+        }],
+        'prefer-template': true
     },
-  };
+};
