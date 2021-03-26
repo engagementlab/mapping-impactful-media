92ebf8def148b0925aee74c7b6daf2339d0d4d83
Johnny Richardson <johnny.c.rich@gmail.com>
2021-03-26 11:22:40 -0400

    refactor(client): cleanup.

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,15 +1,11 @@
 <script lang="ts">
-    // import { Router, Route } from 'svelte-routing';
     import Router from 'svelte-spa-router';
-    import { fly } from 'svelte/transition';
     import Nav from './shared/Nav.svelte';
     import Footer from './shared/Footer.svelte';
     import Home from './home/Home.svelte';
     import About from './about/About.svelte';
     import PageNotFound from './shared/PageNotFound.svelte';
     import Tailwind from './Tailwind.svelte';
-    import Transition from './Transition.svelte';
-    export let url = '';
 
     const routes = { '/': Home, '/about': About, '**': PageNotFound };
 </script>
