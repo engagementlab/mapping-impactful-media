1a91df4de1515f009d083b0e2acf7c9990087cab
Johnny Richardson <johnny.c.rich@gmail.com>
2021-06-03 19:16:15 -0400

    feat: WIP home layout continued.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,31 +7,39 @@
     const res = await getContent(
       fetch,
       `
-     allMimHomePages
-      {
+      allMimHomePages {
         intro
-        about    
+        about
         facebook
         partners
       }
+      allMimPeople(where: { showOnHome: true }) {
+        name
+        title
+        bio
+        image {
+          publicId
+        }
+      }
     `
     );
-    console.log(res);
     return {
       props: {
-        content: res['allMimHomePages'][0],
+        homeContent: res['allMimHomePages'][0],
+        people: res['allMimPeople'],
       },
     };
   }
 </script>
 
 <script>
-  export let content;
+  export let homeContent;
+  export let people;
 </script>
 
-<div class="container mx-auto px-5 flex flex-col-reverse md:flex-row">
+<div class="container md:mx-80 flex flex-col-reverse md:flex-row items-center">
   <h1
-    class="w-full md:w-1/2 font-work-sans font-bold text-orange text-3xl md:text-6xl"
+    class="w-3/4 md:w-1/3 font-work-sans font-bold text-orange text-3xl md:text-6xl"
   >
     Exploring Media literacy practices for just and equitable futures.
   </h1>
@@ -41,17 +49,39 @@
 </div>
 
 <!-- Intro -->
-<div class="container mx-auto p-5 bg-peach md:text-xl">
-  <SvelteMarkdown source={content.intro} />
+<div class="container mx-auto my-10 p-5 bg-peach md:text-xl">
+  <SvelteMarkdown source={homeContent.intro} />
 </div>
 
 <!-- About -->
 <div class="container mx-auto px-5 flex flex-col-reverse md:flex-row">
   <h1 class="w-full md:w-1/2 leading-7">
-    <SvelteMarkdown source={content.about} />
+    <SvelteMarkdown source={homeContent.about} />
   </h1>
   <div>
     <Image imgId="boy-2" width={311} />
+  </div>
+</div>
+
+<!-- Team -->
+<div class="my-20 w-full md:w-1/4 z-10 bg-pink font-work-sans">
+  <h2 class="relative -top-5 text-3xl md:text-5xl">Meet the Team</h2>
+  <div class="md:flex">
+    {#each people as person}
+      <div class="w-full text-center">
+        {person.name}
+        <Image
+          class="w-1/2"
+          imgId={person.image.publicId}
+          width={216}
+          transforms={'c_fill,g_face,w_216,h_217,r_100'}
+          alt={`Headshot image for ${person.name}`}
+        />
+        <p>
+          <SvelteMarkdown source={person.bio} />
+        </p>
+      </div>
+    {/each}
   </div>
 </div>
 
@@ -59,11 +89,11 @@
 <div
   class="relative w-full md:w-1/4 z-10 h-24 font-work-sans bg-yellow bg-opacity-75 text-3xl md:text-5xl"
 >
-  <h2 class="relative -top-5">Partners</h2>
+  <h2 class="relative -top-5 mt-20">Partners</h2>
 </div>
 <div class="relative -top-5 md:w-3/4 p-5 bg-geranium">
   <p class="text-center">
-    <SvelteMarkdown source={content.partners} />
+    <SvelteMarkdown source={homeContent.partners} />
   </p>
   <h3
     class="font-bold p-4 text-2xl text-center
