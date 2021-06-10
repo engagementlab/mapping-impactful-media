73cb0ab285c68a8179e29c72aec2177675d1974d
Johnny Richardson <johnny.c.rich@gmail.com>
2021-06-10 13:16:58 -0400

    feat: Team component, home tweaks.

--- src/lib/Header/index.svelte
+++ src/lib/Header/index.svelte
@@ -2,18 +2,21 @@
   import Image from '$lib/Image.svelte';
 
   const links = [
-    ['/', 'Home'],
+    // ['/', 'Home'],
     ['/about', 'About'],
+    ['/team', 'Team'],
   ];
 </script>
 
 <nav class="container xl:mx-20 my-7 flex flex-row">
   <div class="w-3/4">
-    <Image
-      imgId="logo.png"
-      alt="Logo with text 'Mapping Impactful Media Literacy Practices'"
-      width="305"
-    />
+    <a href="/">
+      <Image
+        imgId="logo.png"
+        alt="Logo with text 'Mapping Impactful Media Literacy Practices'"
+        width="305"
+      />
+    </a>
   </div>
   <div class="flex items-center w-full">
     <ul class="flex justify-around w-full font-work-sans">
---
+++ src/routes/team.svelte
@@ -0,0 +1,26 @@
+<script context="module">
+  import SvelteMarkdown from 'svelte-markdown';
+  import Image from '$lib/Image.svelte';
+  import { getContent } from '$lib/data';
+
+  export async function load({ page, fetch, session, context }) {
+    const res = await getContent(
+      fetch,
+      `allMimAboutPages 
+          {
+              body
+          }`
+    );
+    return {
+      props: {
+        content: res['allMimAboutPages'][0],
+      },
+    };
+  }
+</script>
+
+<script>
+  export let content;
+</script>
+
+<h2>Team - COMING SOON</h2>
