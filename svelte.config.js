b42d800bfbd929108ba3baee4d6fdfe4eb5e121b
Johnny Richardson <johnny.c.rich@gmail.com>
2021-06-30 17:18:44 -0400

    build: svelte config changes.

--- svelte.config.js
+++ svelte.config.js
@@ -4,34 +4,44 @@ import preprocess from 'svelte-preprocess';
 import autoprefixer from 'autoprefixer';
 
 export default {
-	kit: {
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-		}),
-		vite: {
-			mode: 'production',
-			server: {
-				port: 3000,
-				https: true,
-				hmr: false
-			},
-			ssr: {
-				external: ['graphql', 'zen-observable', 'react']
-			},
-			optimizeDeps: {
-				include: ['graphql', 'zen-observable']
-			}
-		}
-	},
-	preprocess: preprocess({
-		scss: {
+  kit: {
 
-			includePaths: ['src'],
-		},
-		postcss: {
-			plugins: autoprefixer,
-		},
-	})
+    target: '#svelte',
+    adapter: adapter({
+      // default options are shown
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+    }),
+    // ssr: false,
+    prerender: {
+      enabled: true,
+      crawl: true,
+	  force: true,
+    },
+    // hydrate: false,
+    vite: {
+      mode: 'production',
+      server: {
+        port: 3000,
+        https: true,
+        hmr: false,
+      },
+      ssr: {
+        external: ['graphql', 'zen-observable', 'react'],
+      },
+      optimizeDeps: {
+        include: ['graphql', 'zen-observable'],
+      },
+    },
+  },
+  preprocess: preprocess({
+    scss: {
+
+      includePaths: ['src'],
+    },
+    postcss: {
+      plugins: autoprefixer,
+    },
+  }),
 };
