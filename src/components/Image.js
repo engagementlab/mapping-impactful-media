fa8e3f42ce41f4bd7d9ff37c042031935259ffaf
Johnny Richardson <johnny.c.rich@gmail.com>
2021-07-14 17:05:24 -0400

    fix: Image component.

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,54 +1,53 @@
 import React, {
-    Component
-} from 'react'
+  Component,
+} from 'react';
 import {
-    Cloudinary
-} from "@cloudinary/base";
+  Cloudinary,
+} from '@cloudinary/base';
 import {
-    AdvancedImage,
-    accessibility,
-    responsive,
-    lazyload
+  AdvancedImage,
+  accessibility,
+  responsive,
+  lazyload,
 } from '@cloudinary/react';
 
 // Cloudinary instance
 const cld = new Cloudinary({
-    cloud: {
-        cloudName: 'engagement-lab-home',
-    },
-    url: {
-        secure: true,
-    },
+  cloud: {
+    cloudName: 'engagement-lab-home',
+  },
+  url: {
+    secure: true,
+  },
 });
 class Image extends Component {
-    constructor() {
-        super()
-    }
-    render() {
+  constructor() {
+    super();
+  }
 
-        // Instantiate a CloudinaryImage object for the image with public ID;
-        // append dir prefix if missing
-        const prefix =
-        this.props.imgId.indexOf('mapping-impactful-media/img/') > -1
-            ? ''
-            : 'mapping-impactful-media/img/';
-        const cloudImage = cld.image(`${prefix}${this.props.imgId}`);
+  render() {
+    // Instantiate a CloudinaryImage object for the image with public ID;
+    // append dir prefix if missing
+    const prefix = this.props.imgId.indexOf('mapping-impactful-media/img/') > -1
+      ? ''
+      : 'mapping-impactful-media/img/';
+    const cloudImage = cld.image(`${prefix}${this.props.imgId}`);
 
-        // Merge classes
-        let allClasses = this.props.className ? this.props.className : '';
-        if (responsive) allClasses = `${allClasses} cld-responsive`;
-                
-        // Create image transforms
-        cloudImage.addTransformation(
-            this.props.transforms || `w_${this.props.width ? this.props.width : 'auto'},f_auto,dpr_auto`
-        );
+    // Merge classes
+    let allClasses = this.props.className ? this.props.className : '';
+    if (responsive) allClasses = `${allClasses} cld-responsive`;
 
-        return (
-            <div>
+    // Create image transforms
+    cloudImage.addTransformation(
+      this.props.transforms || `w_${this.props.width ? this.props.width : 'auto'},f_auto,dpr_auto`,
+    );
+
+    return (
+            <div className={allClasses}>
                 <AdvancedImage cldImg={cloudImage} plugins={[lazyload()]} />
             </div>
-        );
-    }
+    );
+  }
 }
 
 export default Image;
