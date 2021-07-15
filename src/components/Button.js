884afc2bf164414229091f2312ce70e523216f4c
Johnny Richardson <johnny.c.rich@gmail.com>
2021-07-15 12:57:49 -0400

    feat: Button component.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,24 @@
 import React, {
-    Component
-} from "react"
+  Component,
+} from 'react';
+import PropTypes from 'prop-types';
 
 class Button extends Component {
-    render() {
-        return <div> </div>
-    }
+  render() {
+    return (
+        <a
+            href={this.props.link}
+            className={`inline-block border-2 border-black px-10 py-4 my-10 font-bourbon text-sm hover:bg-strawberry hover:text-white transition-all ${this.props.className}`}
+        >
+            {this.props.label}
+          </a>
+    );
+  }
 }
+Button.propTypes = {
+  className: PropTypes.string,
+  link: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+};
 
 export default Button;
