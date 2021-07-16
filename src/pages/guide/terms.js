13b6ec34571835b71fd9c5bc451c4525bb4f7b7e
Johnny Richardson <johnny.c.rich@gmail.com>
2021-07-16 16:09:00 -0400

    feat: more WIP guide/terms page.

--- src/pages/guide/terms.js
+++ src/pages/guide/terms.js
@@ -19,11 +19,11 @@ function GuideTermsPage() {
   return (
     <Layout>
 
- <div className="container mx-auto mt-14 mb-14 xl:mt-48 flex flex-col">
-     <div className="bg-peach rounded-3xl">
+ <div className="container mx-auto mt-14 mb-14 xl:mt-48 flex flex-col max-w-5xl">
+     <div className="bg-peach rounded-55">
 
       <button
-        className="py-6 box-border appearance-none cursor-pointer focus:outline-none flex items-center justify-between"
+        className="py-6 box-border appearance-none cursor-pointer focus:outline-none text-center w-full"
         onClick={toggleAccordion}
         >
         <p className="inline-block text-footnote font-bourbon text-3xl uppercase">what does equity mean to us?</p>
@@ -31,18 +31,9 @@ function GuideTermsPage() {
       <div
         ref={contentSpace}
         style={{ maxHeight: `${height}` }}
-        className="overflow-auto transition-max-height duration-700 ease-in-out"
+        className="overflow-x-auto overflow-y-hidden transition-max-height duration-700 ease-in-out font-work-sans text-3xl px-16"
         >
-        <div className="pb-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mihi enim erit isdem istis fortasse iam utendum. Omnis enim est natura diligens sui. Duo Reges: constructio interrete. Ita multa dicunt, quae vix intellegam.
-
-Summum ením bonum exposuit vacuitatem doloris; An hoc usque quaque, aliter in vita? Quae cum dixisset paulumque institisset, Quid est? Qua tu etiam inprudens utebare non numquam. Nulla erit controversia.
-
-Scisse enim te quis coarguere possit? Quid est enim aliud esse versutum? Illum mallem levares, quo optimum atque humanissimum virum, Cn. Etenim semper illud extra est, quod arte comprehenditur.
-
-Quonam, inquit, modo? Sed quid sentiat, non videtis. Bonum integritas corporis: misera debilitas. Id mihi magnum videtur.
-
-Id Sextilius factum negabat. Illa tamen simplicia, vestra versuta. Confecta res esset. Eam tum adesse, cum dolor omnis absit; Memini vero, inquam; Sint modo partes vitae beatae. Illa videamus, quae a te de amicitia dicta sunt. Quam ob rem tandem, inquit, non satisfacit? Nescio quo modo praetervolavit oratio.
-</div>
+        <div className="pb-10 overflow-hidden">Equity is the quality or value of being impartial, just, and fair. For this field guide, we see equity as a way to frame systemic and structural challenges to creating an impartial, just, and fair experience or environment.  In this guide, the word equity is frequently to refer to processes and goals that align with fairness and justice.</div>
           </div>
       </div>
     </div>
