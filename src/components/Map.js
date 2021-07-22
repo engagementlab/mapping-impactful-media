29ff04c52c3b7c7d00ea6e48c1ba3ed443f204c3
Johnny Richardson <johnny.c.rich@gmail.com>
2021-07-22 17:31:12 -0400

    feat: working first pass of Map.

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,55 +1,101 @@
 import React, { useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import Paper from 'paper';
+import { Point } from 'paper/dist/paper-core';
 
-const Canvas = (props) => {
+const Map = (props) => {
   const canvasRef = useRef(null);
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    canvas.width = 922;
-    canvas.height = 1390;
-    // canvas.width = `${document.getElementById('map-bg').width}px`;
+    canvas.width = props.width;
+    canvas.height = Math.round(props.width * 1.27);
     Paper.setup(canvas);
 
-    const g = new Paper.Group();
-    const dot = new Paper.Shape.Circle({
-      center: [50, 50],
-      radius: 30,
-      fillColor: '#ff0',
+    const w = Paper.view.size.width;
+    const h = Paper.view.size.width;
+    const locations = [
+      {
+        color: `#ffc94b`,
+        center: [w * 0.27, h - h * 0.05],
+      },
+      {
+        color: `#f4751c`,
+        center: [w * 0.17, h - h * 0.6],
+      },
+      {
+        color: `#f29497`,
+        center: [w * 0.45, h - h * 0.92],
+      },
+      { color: `#f2aaac`, center: [w * 0.79, h - h * 0.56] },
+      { color: `#fabb97`, center: [w * 0.75, h - h * 0.17] },
+    ];
+    const bgImg = new Paper.Raster(`map-bg`);
+    bgImg.smoothing = `off`;
+
+    bgImg.on(`load`, () => {
+      locations.forEach((loc, i) => {
+        const g = new Paper.Group();
+        const dot = new Paper.Shape.Circle({
+          center: loc.center,
+          radius: 30 * (w / 600),
+          fillColor: loc.color,
+        });
+        const overlay = new Paper.Path.Circle({
+          center: loc.center,
+          radius: 30 * (w / 600),
+          fillColor: `white`,
+          opacity: 0,
+        });
+
+        const txt = new Paper.PointText({
+          point: new Point(loc.center[0] - 12, loc.center[1] + 20),
+          content: i + 1,
+          fillColor: `black`,
+          fontSize: 40 * (w / 600),
+        });
+        txt.fontFamily = `Bourbon`;
+        txt.fontWeight = `bold`;
+
+        g.addChildren([dot, txt]);
+
+        overlay.onMouseEnter = (event) => {
+          canvas.style.setProperty(`cursor`, `pointer`);
+          dot.tween(
+            { scaling: 1 },
+            { scaling: 1.5 },
+            { duration: 250, easing: `easeInOutQuad` }
+          );
+        };
+        overlay.onMouseLeave = (event) => {
+          canvas.style.setProperty(`cursor`, null);
+          dot.tween({ scaling: 1.5 }, { scaling: 1 }, 250);
+        };
+      });
+      bgImg.position = new Paper.Point(
+        Paper.view.center.x,
+        Paper.view.center.y
+      );
+
+      Paper.view.draw();
     });
-    const overlay = new Paper.Path.Circle({
-      center: [50, 50],
-      radius: 30,
-      fillColor: 'white',
-      opacity: 0.7,
-    });
-
-    const txt = new Paper.PointText({
-      point: [55, 55],
-      content: 1,
-      fillColor: 'black',
-      fontSize: 40,
-    });
-    txt.fontFamily = 'Bourbon';
-    txt.fontWeight = 'bold';
-
-    g.addChildren([dot, txt]);
-
-    overlay.onMouseEnter = (event) => {
-      //   canvas.style.setProperty('cursor', 'pointer');
-      dot.tween({ scaling: 1 }, { scaling: 2 }, { duration: 250, easing: 'easeInOutQuad' });
-    };
-    overlay.onMouseLeave = (event) => {
-      canvas.style.setProperty('cursor', null);
-      dot.tween({ scaling: 2 }, { scaling: 1 }, 250);
-    };
-    const raster = new Paper.Raster('map-bg', new Paper.Point(50, 50));
-
-    // raster.rescale(200, 100);
-    Paper.view.draw();
   }, []);
 
-  return <canvas ref={canvasRef} {...props} id="map-canvas" resize="true" />;
+  return (
+    <div>
+      <img
+        id="map-bg"
+        className="hidden"
+        aria-hidden="true"
+        src={
+          `https://res.cloudinary.com/engagement-lab-home/image/upload/c_scale,h_` +
+          Math.round(props.width * 1.27) +
+          `/v1626969494/mapping-impactful-media/img/map`
+        }
+      />
+      <canvas ref={canvasRef} id="map-canvas" resize="true" />
+    </div>
+  );
 };
 
-export default Canvas;
+export default Map;
--- src/pages/guide/map.js
+++ src/pages/guide/map.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import Image from '../../components/Image';
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+
 import Layout from '../../components/Layout';
 import Map from '../../components/Map';
 
 function GuideMapPage() {
+  const [mapActive, setMapActive] = useState(false);
+  const [mapWidth, setMapWidth] = useState(false);
+  const [mapHeight, setMapHeight] = useState(false);
+  useEffect(() => {
+    window.setTimeout(() => {
+      const width = document.querySelector('#map-container').clientWidth;
+      const height = document.querySelector('#map-container').clientHeight;
+      setMapWidth(width);
+      setMapHeight(height);
+      setMapActive(true);
+    }, 100);
+  });
+
   return (
     <Layout>
-        <div className="container mx-auto mt-14 mb-14 xl:mt-48">
-
-            <div className="absolute">
-
-            <Image
-                id="map-bg"
-                imgId="map"
-                alt="A cartoonish drawing of a small town"
-                width={922}
-            />
-            </div>
-            <div className="absolute">
-                <Map />
-            </div>
+      <div className="container mx-auto mt-14 mb-14 xl:mt-48">
+        <div className="flex">
+          <div className="font-work-sans text-5xl w-1/2">
+            In the map, you have five places to visit. (blurb or directions on
+            interacting with map?)
+          </div>
+          <div id="map-container" className="w-1/2">
+            {mapActive && <Map width={mapWidth} height={mapHeight} />}
+          </div>
         </div>
+      </div>
     </Layout>
   );
 }
