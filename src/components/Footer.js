0a9a432901a054cb48fc40123f05f3f1ff836eaa
Johnny Richardson <johnny.c.rich@gmail.com>
2021-07-28 17:46:56 -0400

    fix: small Header component layout fix,

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,23 @@
-import React, {
-    Component
-} from "react"
+import React, { Component } from 'react';
 
 class Footer extends Component {
-    render() {
-        return (
-            <footer className="flex justify-center w-full p-10 bg-sunny">
-              <div className="w-full md:w-1/2">
-                <h3 className="font-bourbon">Mapping Impactful Media Literacy Practices</h3>
-                <p className="pt-5 font-work-sans">
-                  Contact: Paul Mihailidis<br />paul_mihailidis@emerson.edu ​ ​
-                </p>
-                <ul className="pt-5 font-work-sans">
-                </ul>
-              </div>
-           </footer>
-        );
-    }
+  render() {
+    return (
+      <footer className="flex justify-center w-full p-10 mt-16 bg-sunny">
+        <div className="w-full md:w-1/2">
+          <h3 className="font-bourbon">
+            Mapping Impactful Media Literacy Practices
+          </h3>
+          <p className="pt-5 font-work-sans">
+            Contact: Paul Mihailidis
+            <br />
+            paul_mihailidis@emerson.edu ​ ​
+          </p>
+          <ul className="pt-5 font-work-sans"></ul>
+        </div>
+      </footer>
+    );
+  }
 }
 
 export default Footer;
