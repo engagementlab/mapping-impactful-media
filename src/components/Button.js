e4a903e980fb548eda4c43ca0452dc88a8df40ff
Johnny Richardson <johnny.c.rich@gmail.com>
2021-07-28 17:46:23 -0400

    feat: add "small" prop to Button component.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,17 @@
-import React, {
-  Component,
-} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Button extends Component {
   render() {
     return (
-        <a
-            href={this.props.link}
-            className={`inline-block border-2 border-black px-10 py-4 my-10 font-bourbon text-sm hover:bg-strawberry hover:text-white transition-all ${this.props.className}`}
-        >
-            {this.props.label}
-          </a>
+      <a
+        href={this.props.link}
+        className={`inline-block border-2 border-black px-10 py-4 my-10 font-bourbon text-sm hover:bg-strawberry hover:text-white transition-all ${
+          this.props.className
+        } ${this.props.small ? `` : `text-3xl py-6 tracking-widest`}`}
+      >
+        {this.props.label}
+      </a>
     );
   }
 }
@@ -19,6 +19,7 @@ Button.propTypes = {
   className: PropTypes.string,
   link: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
+  small: PropTypes.bool,
 };
 
 export default Button;
