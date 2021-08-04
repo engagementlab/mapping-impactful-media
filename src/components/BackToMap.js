60422ab513846b243193ba66225372557b74da9d
Johnny Richardson <johnny.c.rich@gmail.com>
2021-08-04 18:37:23 -0400

    feat: back to map button.

---
+++ src/components/BackToMap.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import mapIcon from '../images/map.png';
+
+class BackToMap extends Component {
+  render() {
+    return (
+      <a href="/guide/map" className="group flex items-center">
+        <span className="inline-block px-6 text-3xl text-pewter uppercase group-hover:text-sunny">
+          back to map
+        </span>
+        <img
+          src={mapIcon}
+          className="inline-block transform transition-transform ease-in-out group-hover:scale-110"
+        />
+      </a>
+    );
+  }
+}
+
+export default BackToMap;
