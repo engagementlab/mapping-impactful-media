3aeb189cc7c98207c41a19ccf1440157cf1a943d
Johnny Richardson <johnny.c.rich@gmail.com>
2021-08-12 16:26:35 -0400

    build: make all routes scroll user to top of page.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,11 @@
 import './src/styles/global.css';
+module.exports = {
+  shouldUpdateScroll: ({
+    routerProps: { location },
+    getSavedScrollPosition,
+  }) => {
+    window.scrollTo(0, 0);
+
+    return false;
+  },
+};
