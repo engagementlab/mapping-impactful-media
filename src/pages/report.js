a050790661a10e9e38921c9e1c20c17f49a4ab3f
Johnny Richardson <johnny.c.rich@gmail.com>
2021-08-27 14:56:00 -0400

    feat: report page WIP.

--- src/pages/report.js
+++ src/pages/report.js
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 
 import Layout from '../components/Layout';
+import ReactMarkdown from 'react-markdown';
+import Button from '../components/Button';
 
 function ReportPage() {
   const content = useStaticQuery(graphql`
@@ -9,9 +11,6 @@ function ReportPage() {
       elApi {
         allMimReportPages {
           blurb
-          image {
-            publicId
-          }
           reportFile {
             publicUrl
           }
@@ -22,7 +21,25 @@ function ReportPage() {
 
   return (
     <Layout>
-      <div id="report">Coming soon</div>
+      <div
+        id="report"
+        className="container mx-auto mt-14 mb-14 xl:mt-48 flex md:flex-row items-center font-work-sans text-xl md:text-2xl"
+      >
+        <div className="w-1/3">Images here</div>
+        <div className="flex flex-col justify-center mx-12 text-center">
+          <h2 className="font-bourbon text-3xl max-w-xl">
+            Mapping Impactful Media Literacy Practices: The Report
+          </h2>
+          <ReactMarkdown>
+            {content.elApi.allMimReportPages[0].blurb}
+          </ReactMarkdown>
+          <Button
+            label="Download Full Report"
+            link={content.elApi.allMimReportPages[0].reportFile.publicUrl}
+          />
+        </div>
+        <div className="w-1/3">Images here</div>
+      </div>
     </Layout>
   );
 }
