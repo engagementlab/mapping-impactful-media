72984b3a629dfab9bd57f9d881312a305880bcf8
Johnny Richardson <johnny.c.rich@gmail.com>
2021-09-24 13:51:18 -0400

    fix: guide/start layout re-work.

--- src/pages/guide/start.js
+++ src/pages/guide/start.js
@@ -27,52 +27,55 @@ function GuideIndexPage() {
   return (
     <Layout>
       <div
-        className="flex flex-col lg:flex-row
+        className="flex flex-col
       "
       >
         <div className="lg:w-2/5 flex-shrink-0">
           <SubHeader text="Our Vision, Our Values" color="bg-peach" />
+        </div>
+        <div
+          id="guide-start"
+          className="flex flex-col items-center font-work-sans text-xl md:text-2xl
+      "
+        >
+          <ReactMarkdown className="lg:w-2/3">
+            {content.startText1}
+          </ReactMarkdown>
           <Image
             id="girl-img"
             imgId="girl-2"
             width={753}
             alt={`A cartoonish drawing of the side-view of the bottom three-quarters of a woman's head. On the other top quarter above her head are drawings of a wifi signal symbol, flowers and plants, and circles, as well as a red thought bubble that has the words "How does this make me feel?" in it.`}
           />
-        </div>
-        <div
-          id="guide-start"
-          className="px-5 font-work-sans text-xl md:text-2xl
-      "
-        >
-          <ReactMarkdown>{content.startText1}</ReactMarkdown>
-          <div className="bg-orange bg-opacity-25 my-16 py-2 px-2 xl:mx-24 xl:py-8 md:px-16">
+          <div className="bg-orange bg-opacity-25 my-16 py-2 xl:py-8 md:px-16">
             <ReactMarkdown>{content.startTextItems}</ReactMarkdown>
           </div>
-          <ReactMarkdown>{content.startText2}</ReactMarkdown>
+          <ReactMarkdown className="lg:w-2/3">
+            {content.startText2}
+          </ReactMarkdown>
+          <div
+            className="flex flex-col mb-14 lg:mt-12 text-xl md:text-2xl font-work-sans w-2/3
+      "
+          >
+            <ReactMarkdown>{content.startText3}</ReactMarkdown>
+            <div
+              id="guide-intentions"
+              className="lg:mt-12 font-work-sans text-xl md:text-2xl
+      "
+            >
+              <div className="bg-orange bg-opacity-25 py-8 px-2 xl:py-8 md:px-16">
+                <h3 className="font-bourbon uppercase text-3xl pt-8">
+                  The following intentions drove the creation of this field
+                  guide:
+                </h3>
+                <br />
+                <ReactMarkdown>{content.startTextIntentions}</ReactMarkdown>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div
-        className="flex flex-col xl:flex-row mb-14 lg:mt-12 text-xl md:text-2xl font-work-sans
-      "
-      >
-        <div className="xl:w-2/6 flex-shrink-0 px-5 lg:ml-16">
-          <ReactMarkdown>{content.startText3}</ReactMarkdown>
-        </div>
-        <div
-          id="guide-intentions"
-          className="px-5 lg:mt-12 font-work-sans text-xl md:text-2xl
-      "
-        >
-          <div className="bg-orange bg-opacity-25 py-8 px-2 xl:mx-24 xl:py-8 md:px-16">
-            <h3 className="font-bourbon uppercase text-3xl pt-8">
-              The following intentions drove the creation of this field guide:
-            </h3>
-            <br />
-            <ReactMarkdown>{content.startTextIntentions}</ReactMarkdown>
-          </div>
-        </div>
-      </div>
       <div className="text-center mt-16">
         <Button label="Continue" link="/guide/faq" />
       </div>
