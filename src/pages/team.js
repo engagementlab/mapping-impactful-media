3a05c8eac9400876d433fc8cec802813a253a10b
Johnny Richardson <johnny.c.rich@gmail.com>
2021-09-29 11:38:20 -0400

    fix: updated team section order.

--- src/pages/team.js
+++ src/pages/team.js
@@ -95,28 +95,32 @@ function TeamPage() {
           </div>
         </div>
 
-        {/* NAMLE */}
-        <div className="absolute w-full md:right-0 md:w-4/6 lg:right-48 lg:w-3/6 xl:w-2/6 md:mt-14 h-44 font-work-sans bg-strawberry text-3xl md:text-4xl lg:text-5xl">
-          <h2 className="relative mt-40 md:p-3 md:top-14 md:mt-20 lg:top-12">
-            NAMLE Team
+        {/* Design */}
+        <div className="absolute w-full md:right-0 md:w-4/6 lg:right-48 lg:w-3/6 xl:w-2/6 md:mt-14 h-44 font-work-sans bg-bee text-3xl md:text-4xl lg:text-5xl">
+          <h2 className="relative md:-left-7 md:top-16 md:mt-20 md:pt-2 lg:pt-0">
+            Design Team
           </h2>
         </div>
         <div
-          id="people-namle"
+          id="people-design"
           className="flex flex-col justify-center mt-64 md:mt-72"
         >
-          <p className="mx-auto w-11/12 lg:ml-96 lg:w-2/6 lg:text-right xl:ml-auto xl:mr-64 leading-7">
-            The project is supported by the National Association of Media
-            Literacy Education, whose leaders help coordinate our process.
+          <p className="mx-auto w-11/12 lg:ml-96 lg:w-1/2 xl:w-2/6 lg:text-right xl:ml-auto xl:mr-64 leading-7">
+            The design team consists of a group of professional graphic
+            designers and developers who work to pull together the final product
+            for the research. Many of the team members are also graduate
+            students interested in using their professional talents to integrate
+            social justice and media literacy initiatives within their
+            respective fields
           </p>
           <div className="mx-auto w-11/12 xl:w-1/2 mt-10">
-            {peopleRender(peopleNamle, `namle`)}
+            {peopleRender(peopleDesign, `design`)}
           </div>
         </div>
 
         {/* Board */}
         <div className="relative w-full md:ml-52 lg:ml-64 md:w-4/6 lg:w-1/4 h-44 font-work-sans bg-peach text-3xl md:text-4xl xl:text-5xl">
-          <h2 className="relative top-36 pt-3 md:left-64 md:pl-2 lg:left-64 lg:ml-20 md:mt-20">
+          <h2 className="relative top-36 pt-3 md:left-64 md:pl-2 lg:left-64 lg:ml-14 md:mt-20">
             Advisory Board
           </h2>
         </div>
@@ -132,25 +136,22 @@ function TeamPage() {
         </div>
       </div>
 
-      {/* Design */}
-      <div className="absolute w-full md:right-0 md:w-4/6 lg:right-48 lg:w-3/6 xl:w-2/6 md:mt-14 h-44 font-work-sans bg-bee text-3xl md:text-4xl lg:text-5xl">
-        <h2 className="relative md:-left-7 md:top-16 md:mt-20 md:pt-2 lg:pt-0">
-          Design Team
+      {/* NAMLE */}
+      <div className="absolute w-full md:right-0 md:w-4/6 lg:right-48 lg:w-3/6 xl:w-2/6 md:mt-14 h-44 font-work-sans bg-strawberry text-3xl md:text-4xl lg:text-5xl">
+        <h2 className="relative mt-40 md:p-3 md:top-14 md:mt-20 lg:top-12">
+          NAMLE Team
         </h2>
       </div>
       <div
-        id="people-design"
+        id="people-namle"
         className="flex flex-col justify-center mt-64 md:mt-72"
       >
-        <p className="mx-auto w-11/12 lg:ml-96 lg:w-1/2 xl:w-2/6 lg:text-right xl:ml-auto xl:mr-64 leading-7">
-          The design team consists of a group of professional graphic designers
-          and developers who work to pull together the final product for the
-          research. Many of the team members are also graduate students
-          interested in using their professional talents to integrate social
-          justice and media literacy initiatives within their respective fields
+        <p className="mx-auto w-11/12 lg:ml-96 lg:w-2/6 lg:text-right xl:ml-auto xl:mr-64 leading-7">
+          The project is supported by the National Association of Media Literacy
+          Education, whose leaders help coordinate our process.
         </p>
         <div className="mx-auto w-11/12 xl:w-1/2 mt-10">
-          {peopleRender(peopleDesign, `design`)}
+          {peopleRender(peopleNamle, `namle`)}
         </div>
       </div>
 
